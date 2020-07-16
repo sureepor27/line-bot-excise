cafa05602de550c6ae153d6f6f8d29f054d5a3ba
sureepor27 <68370016+sureepor27@users.noreply.github.com>
2020-07-16 14:56:05 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'UAaZWrcVaIjkEPKA9fTnKRuy2FdmemHx5tkpgtFbHZ0EZZwzq91zC2LtTd+ORlBwZzBoibkjDeEx8z8O8Qa/ai9mYQOARfja/k4UocqIV9zKRBxsMVV7uwT2+AoxtFsGIFWToa9Rsvqg/Ej3L8yXXQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
